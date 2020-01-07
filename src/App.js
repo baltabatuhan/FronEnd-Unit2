da74121d66b9d1c56c6fb52ff9092f4fc012562f
Batuhan Balta <bthn.balta@gmail.com>
2020-01-06 18:56:42 -0800

    upgrades

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import Navbar from "./Navbar";
-import Content from "./Content";
+import React from "react";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Navbar />
-        <Content />
-      </div>
-    );
-  }
-}
+import { Route } from "react-router-dom";
+import "./styles.css";
+import HomePage from "./component/HomePage";
+import Stories from "./component/Stories";
+import SubmitStories from "./component/SubmitStories";
+import Login from "./component/Login";
+import Navigation from "./component/Navigation";
+import Footer from "./component/Footer";
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <h1 className="header">Refugee Stories</h1>
+        <div className="nav-links">
+          <Navigation />
+        </div>
+      </nav>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/stories" component={Stories} />
+      <Route path="/submit" component={SubmitStories} />
+      <Route path="/login" component={Login} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
